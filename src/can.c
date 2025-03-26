29ae428b9efaafd9246de97500459cac14cf89b3
Giancarlo Bonvenuto <ra248527@beatles.lab.ic.unicamp.br>
2025-03-26 17:36:38 -0300

    terminando de receber as mensagens que precisa

--- src/can.c
+++ src/can.c
@@ -19,62 +19,77 @@ struct can_timing timing;
 
 bool mensagem_recebida = false;
 static void rx_callback_function(const struct device *dev,
-                                 struct can_frame *frame, void *user_data) {
+                                 struct can_frame *frame, void *user_data)
+{
 
-  if (frame == NULL) {
+  if (frame == NULL)
+  {
     LOG_ERR("Null frame recebido");
     return;
   }
 
-  if (mensagem_recebida) {
+  if (mensagem_recebida)
+  {
     LOG_ERR("Já estamos lidando com uma mensagem, ignorando esta");
   }
+  // Tensão da Bateria
 
-  //TPMS
-  if (frame->id == 0x07 /* DE, DD*/) {
-    pneu_de = frame->data_32[0];
-    pneu_dd = frame->data_32[1];
+  if (frame->id == 0x04 /*Volts*/)
+  {
+    bateria = (frame->data_32[0] / CONFIG_BATERIA_MAX) * 100;
   }
-  if (frame->id == 0x08 /* TE, TD*/) {
-    pneu_te = frame->data_32[0];
-    pneu_td = frame->data_32[1];
+  // ENND Tensão da Bateria
+  //  TPMS
+  if (frame->id == 0x07 /* DE, DD*/)
+  {
+    pneu_de = (frame->data_32[0] / CONFIG_PRESSAO_MAX) * 100;
+    pneu_dd = (frame->data_32[1] / CONFIG_PRESSAO_MAX) * 100;
   }
-  //END TPMS
+  if (frame->id == 0x08 /* TE, TD*/)
+  {
+    pneu_te = (frame->data_32[0] / CONFIG_PRESSAO_MAX) * 100;
+    pneu_td = (frame->data_32[1] / CONFIG_PRESSAO_MAX) * 100;
+  }
+  // END TPMS
 
   mensagem_recebida = true;
 
   LOG_INF("Recebi a mensagem %d do id %d", frame->data[0], frame->id);
 }
 
-
 // Aceitando todos os CAN
 const struct can_filter my_filter = {
     .flags = 0U, .id = 0x000, .mask = CAN_STD_ID_MASK};
 int filter_id;
 
-int Init_CAN() {
+int Init_CAN()
+{
   int ret;
   LOG_INF("Inicializando");
 
   ret = can_set_mode(can_dev, CAN_MODE_NORMAL);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     printf("Error setting CAN mode [%d]", ret);
     return 0;
   }
 
   ret = can_set_bitrate(can_dev, 125000);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     LOG_ERR("Failed to set timing (%d)", ret);
   }
 
   ret = can_start(can_dev);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     printf("Error starting CAN controller [%d]", ret);
     return 0;
   }
   filter_id =
       can_add_rx_filter(can_dev, rx_callback_function, NULL, &my_filter);
-  if (filter_id < 0) {
+  if (filter_id < 0)
+  {
     LOG_ERR("Unable to add rx filter [%d]", filter_id);
   }
 
